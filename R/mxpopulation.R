211a971b881ae9efc53ae9e721cbeea030487c84
Eduardo Flores <eduardo@enelmargen.org>
2018-01-16 10:39:01 +0100

    changed some names to english, coded gender

--- R/mxpopulation.R
+++ R/mxpopulation.R
@@ -1,6 +1,6 @@
 #' Population estimates 2010 to 2030
 #'
-#' A dataset population estimates by municipality level in Mexico. Public data from CONAPO.
+#' A dataset of population estimates by municipality level in Mexico. Public data from CONAPO.
 #' Obtained Jan-15-2018, special characters overwritten for compatibility.
 #'
 #' @format A data frame with 515,970 rows and 9 variables:
@@ -10,8 +10,8 @@
 #'   \item{id_st}{state id, as defined by INEGI}
 #'   \item{mun}{municipio, in full name}
 #'   \item{id_mun}{municipio id, as defined by INEGI}
-#'   \item{cvegeo}{geographic id, defined as state id plus municipio id}
-#'   \item{gender}{gender}
+#'   \item{geoid}{geographic id, defined as state id plus municipio id}
+#'   \item{gender}{gender. Cero for males, one for females}
 #'   \item{age}{age group}
 #'   \item{population}{population}
 #' }
